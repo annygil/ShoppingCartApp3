a5a78cad6b7271cf333bf68ed00f7e76344e68a6
UBITICA <annyvanessag@gmail.com>
2015-08-10 13:01:06 -0430

    Segundo test funcionando

--- src/test/java/com/mycompany/shoppingcartapp3/ShoppingCart.java
+++ src/test/java/com/mycompany/shoppingcartapp3/ShoppingCart.java
@@ -4,19 +4,24 @@
  * and open the template in the editor.
  */
 package com.mycompany.shoppingcartapp3;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 /**
  *
  * @author ubitica-01
  */
 public class ShoppingCart {
 
+ 
+	private List<Product> productList = new ArrayList();
+	
 	public int getProductCount() {
-		return 0;
+		return productList.size();
 	}
  
 	public void addProduct(Product product) {
-		
+		productList.add(product);
 	}
     
 }
